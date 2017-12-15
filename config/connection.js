83a56a8cb1b80a2f57c92510b86a66b5bc11d657
Matt Quade <quadem09@gmail.com>
2017-12-15 20:01:14 +0000

    second unfinished app

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 //establishing connection to mySQL
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    //change before deploying to heroku
-    host: 'localhost',
-    user: 'root',
-    password: "",
-    database: "burgers_db"
-});
+var connection;
+// var connection = mysql.createConnection({
+//     //change before deploying to heroku
+//     host: 'localhost',
+//     user: 'root',
+//     password: "",
+//     database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "k0nga33r4susw6t8",
+        password: "h5jo1xdvfthysv7i",
+        database: "gfcelxbnh92xwcyn"
+    });
+};
+
 
 connection.connect(function(err) {
     if (err) {
