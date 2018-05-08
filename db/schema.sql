9155c1e0384539d0bf091bd95bcb791eef80fcbb
mquade91 <quadem09@gmail.com>
2018-05-08 12:14:55 -0400

    last push to make sure up to date

--- db/schema.sql
+++ db/schema.sql
@@ -2,15 +2,17 @@
 -- mySQL command
 CREATE DATABASE burgers_db;
 USE burgers_db;
-
 -- JAWs DB command
--- USE gfcelxbnh92xwcyn;
-
+USE gfcelxbnh92xwcyn;
 -- creating table
-CREATE TABLE burgers (
-id int AUTO_INCREMENT,
-burger_name varchar (30) NOT NULL,
-devoured BOOLEAN NOT NULL default 1,
-time TIMESTAMP,
-PRIMARY KEY(id)
-)
+CREATE TABLE burgers
+(
+  id int NOT NULL
+  AUTO_INCREMENT,
+burger_name varchar
+  (30) NOT NULL,
+devoured BOOLEAN default 1,
+date NOT NULL TIMESTAMP ,
+PRIMARY KEY
+  (id)
+);
